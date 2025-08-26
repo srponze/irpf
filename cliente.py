73d90e4bd4c752b0b3071fc5746c6fee5c4831f4
Jose Ponce <joseponcegar@gmail.com>
2025-08-26 18:52:38 +0200

    docs(cliente): explicación de opciones en cliente.py

--- cliente.py
+++ cliente.py
@@ -8,13 +8,16 @@ manager = Manager(
     transactionPath=transactionPath,
     accountPath=accountPath,
     añoRenta=añoRenta,
-    realizarAcciones=True,
-    agruparAcciones=False,
-    imprimirMovAcciones=True,
-    imprimirTransAcciones=True,
-    realizarDivisas=True,
-    imprimirMovDivisas=True,
-    imprimirTransDivisas=True,
-    introducirRenta=False,
-    modoSimulador=True,
+    realizarAcciones=True,  # Realiza los calculos de acciones
+    agruparAcciones=False,  # Agrupa las acciones en solo una transaccion por valor, sumando todas las compras y ventas de dicho valor
+    imprimirMovAcciones=True,  # Imprime los movimientos de acciones
+    imprimirTransAcciones=True,  # Imprime las transacciones de acciones
+    realizarDivisas=True,  # Realiza los calculos de divisas
+    imprimirMovDivisas=True,  # Imprime los movimientos de divisas
+    imprimirTransDivisas=True,  # Imprime las transacciones de divisas
+    # Introduccion de ganancias en Renta Web
+    introducirRenta=True,  # Introduce los datos en la web de la renta
+    modoSimulador=False,  # Introduce los datos en una simulacion de Renta Web, en vez de iniciar sesion
+    headless=False,  # Modo sin interfaz grafica
+    tiempoDeEspera=150,  # Tiempo de espera (ms) para que se introduzcan los datos correctamente, si falla aumentar tiempo
 )
